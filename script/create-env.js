bd68cbf979c1018e2a08564c85737c3fc4f1ed75
Rodrigo Zucchini <rodrigozucchini1997@gmail.com>
2021-07-28 01:07:16 -0300

    cambios nuevos 18

--- script/create-env.js
+++ script/create-env.js
@@ -1,4 +1,3 @@
-const fs = require('fs'); // fs = file system
+const fs = require('fs');
 
-// fs.writeFileSync("path", `argumento a crear`);
-fs.writeFileSync("./.env", `API=${process.env.API}\n`);
+	fs.writeFileSync('./.env',`API=${process.env.API}\n`)
